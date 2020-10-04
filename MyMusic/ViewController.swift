f968e61c50a3cc4b61428922ab914b029f2d515b
rkdwlss2 <rkdwlss2@gmail.com>
2020-10-04 10:33:33 +0900

    Initial Commit

---
+++ MyMusic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMusic
+//
+//  Created by 강명진 on 2020/10/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
