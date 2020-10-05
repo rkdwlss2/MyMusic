92193b1c2be2b6581a2c9f76104f0e8b7095367e
rkdwlss2 <rkdwlss2@gmail.com>
2020-10-05 11:20:48 +0900

    music app

---
+++ MyMusic/PlayerViewController.swift
@@ -0,0 +1,216 @@
+//
+//  PlayerViewController.swift
+//  MyMusic
+//
+//  Created by 강명진 on 2020/10/04.
+//
+import AVFoundation
+import UIKit
+
+class PlayerViewController: UIViewController {
+
+    public var position: Int=0;
+    public var songs: [Song]=[]
+    
+    @IBOutlet var holder: UIView!
+    
+    var player: AVAudioPlayer?
+    //u i e
+    private let albumImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    
+    private let songNameLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        return label
+    }()
+    private let artistNameLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        return label
+    }()
+    private let albumNameLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    let playPauseButton = UIButton()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        if holder.subviews.count == 0{
+            configure()
+        }
+    }
+    func configure(){
+        //set up player
+        let song = songs[position]
+        let urlString = Bundle.main.path(forResource: song.trackName, ofType: "mp3")
+      
+        
+        do{
+            try AVAudioSession.sharedInstance().setMode(.default)
+            try AVAudioSession.sharedInstance().setActive(true, options: .notifyOthersOnDeactivation)
+            guard let urlString = urlString else{
+                print("urlstring is nil")
+                return
+            }
+            player = try AVAudioPlayer(contentsOf: URL(string: urlString)!)
+            
+            guard let player = player else {
+                print("player is nill")
+                return
+            }
+            player.volume=0.5
+            
+            player.play()
+        }catch{
+            print("error occured")
+        }
+        //set tup user interface elements
+        
+        //album cover
+        albumImageView.frame=CGRect(x:10,y:10,
+                                    width: holder.frame.size.width-20,
+                                    height: holder.frame.size.width-20
+        )
+        albumImageView.image=UIImage(named: song.imageName)
+        holder.addSubview(albumImageView)
+        
+        
+        songNameLabel.frame=CGRect(x:10,y:albumImageView.frame.size.height+10,
+                                    width: holder.frame.size.width-20,
+                                    height: 70
+        )
+        albumNameLabel.frame=CGRect(x:10,y:albumImageView.frame.size.height+10+70,
+                                    width: holder.frame.size.width-20,
+                                    height: 70
+        )
+        artistNameLabel.frame=CGRect(x:10,y:albumImageView.frame.size.height+10+70+70,
+                                    width: holder.frame.size.width-20,
+                                    height: 70
+        )
+        
+        songNameLabel.text=song.name
+        albumNameLabel.text=song.albumName
+        artistNameLabel.text=song.artistName
+        
+        //Labels
+        holder.addSubview(songNameLabel)
+        holder.addSubview(albumNameLabel)
+        holder.addSubview(artistNameLabel)
+
+        
+        //Player controls
+        
+        let nextButton = UIButton()
+        let backButton = UIButton()
+
+        //Frame
+        let yPosition = artistNameLabel.frame.origin.y+70+20
+        let size: CGFloat=70
+        
+        playPauseButton.frame = CGRect(x: (holder.frame.size.width-size)/2.0, y: yPosition, width: size, height: size)
+        nextButton.frame = CGRect(x: (holder.frame.size.width-size-20), y: yPosition, width: size, height: size)
+        backButton.frame = CGRect(x: 20, y: yPosition, width: size, height: size)
+
+        
+        //add action
+        playPauseButton.addTarget(self, action: #selector(didTapPlayPauseButton), for: .touchUpInside)
+        nextButton.addTarget(self, action: #selector(didTapNextButton), for: .touchUpInside)
+        backButton.addTarget(self, action: #selector(didTapBackButton), for: .touchUpInside)
+
+        
+        //styling
+        playPauseButton.setBackgroundImage(UIImage(systemName: "pause.fill"), for: .normal)
+        backButton.setBackgroundImage(UIImage(systemName: "backward.fill"), for: .normal)
+        nextButton.setBackgroundImage(UIImage(systemName: "forward.fill"), for: .normal)
+
+        playPauseButton.tintColor = .black
+        backButton.tintColor = .black
+        nextButton.tintColor = .black
+
+        
+        holder.addSubview(playPauseButton)
+        holder.addSubview(nextButton)
+        holder.addSubview(backButton)
+
+        //slider
+        let slider = UISlider(frame: CGRect(x:20,y:holder.frame.size.height-60,
+                                            width: holder.frame.size.width-40,
+                                            height: 50))
+        
+        slider.value=0.5
+        slider.addTarget(self, action: #selector(didSlideSlider(_:)), for: .valueChanged)
+        holder.addSubview(slider)
+    }
+    @objc func didTapBackButton(){
+        if position > 0 {
+            position=position-1
+            player?.stop()
+            for subview in holder.subviews{
+                subview.removeFromSuperview()
+            }
+            configure()
+        }
+    }
+    @objc func didTapNextButton(){
+        if position < (songs.count-1) {
+            position=position+1
+            player?.stop()
+            for subview in holder.subviews{
+                subview.removeFromSuperview()
+            }
+            configure()
+        }
+    }
+    @objc func didTapPlayPauseButton(){
+        if player?.isPlaying==true{
+            player?.pause()
+            
+            playPauseButton.setBackgroundImage(UIImage(systemName: "play.fill"), for: .normal)
+            // shrink image
+            UIView.animate(withDuration: 0.2, animations: {
+                self.albumImageView.frame = CGRect(x: 30,
+                                                   y: 30,
+                                                   width: self.holder.frame.size.width-60,
+                                                   height: self.holder.frame.size.width-60)
+            })
+            
+        }
+        else{
+            player?.play()
+            
+            playPauseButton.setBackgroundImage(UIImage(systemName: "pause.fill"), for: .normal)
+            UIView.animate(withDuration: 0.2, animations: {
+               self.albumImageView.frame = CGRect(x: 10,
+                                             y: 10,
+                                             width: self.holder.frame.size.width-20,
+                                             height: self.holder.frame.size.width-20)
+           })
+        }
+    }
+    
+    @objc func didSlideSlider(_ slider: UISlider){
+        let  value = slider.value
+        player?.volume=value
+        
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+            super.viewWillDisappear(animated)
+            if let player = player {
+                player.stop()
+            }
+        }
+}
